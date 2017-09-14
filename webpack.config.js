44bd1dc09e2edfea721c824b5be2d105e633bcc4
Devillers Jerome <jerome@Devillerss-MacBook-Pro.local>
2017-09-14 16:01:53 -0400

    Update Package

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
+const nodeExternals = require('webpack-node-externals');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
+    target: 'node', // in order to ignore built-in modules like path, fs, etc. 
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder 
     entry: "./index.js",
     output: {
         path: __dirname,
         filename: "dist/bundle.js"
     },
     module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+                options: {
+                    includePaths: ["src/sass/admin.scss", "dist/style.css"]
+                }
+            }]
+        }],
         loaders: [
             { test: /\.css$/, loader: "style!css" },
-            { test: /\.json$/, loader: 'json-loader' }
+            { test: /\.json$/, loader: 'json-loader' },
+            { test:   /\.(scss|sass|css)$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+            { test: /\.scss$/, loader: 'style!css!sass!sass-resources'}
         ]
     },
+    plugins: [
+      new ExtractTextPlugin("[name].css")
+    ],
     node: {
-        console: false,
+        console: true,
         fs: "empty"
     }
 };
